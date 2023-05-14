f40dbeb8d322f2b04590f0ca0fed821eba785ff7
michimel <126293937+michimel@users.noreply.github.com>
2023-05-14 14:17:48 +0000

    Book:attributes,get-/setters,constructor,toString

--- elib/src/main/java/ch/fhnw/elibrary/elib/data/domain/Book.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/data/domain/Book.java
@@ -1,5 +1,118 @@
 package ch.fhnw.elibrary.elib.data.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private int year;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String genre;
+
+    // constructors
+    public Book(Long id, String isbn, String title, int year, String description, String author, String genre) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.year = year;
+        this.description = description;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                ", description='" + description + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+
+        
+
+
+        
     
 }
