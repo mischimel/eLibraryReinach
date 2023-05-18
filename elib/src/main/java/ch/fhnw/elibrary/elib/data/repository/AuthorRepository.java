72ee9c36c7a06321ceadfa04f572b1058023547b
michimel <126293937+michimel@users.noreply.github.com>
2023-05-18 09:53:56 +0000

    AuthorRepository

--- elib/src/main/java/ch/fhnw/elibrary/elib/data/repository/AuthorRepository.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/data/repository/AuthorRepository.java
@@ -1,5 +1,7 @@
 package ch.fhnw.elibrary.elib.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,11 @@ import ch.fhnw.elibrary.elib.data.domain.Author;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
+    /* custom query methods
+    - methods with type Author will only return one author (first one found)
+    - methods with type List<Author> will return a list with all authors found */
+    List<Author> findByFirstName(String firstName);
+    List<Author> findByLastName(String lastName);
+    Author findByFirstNameAndLastName(String firstName, String lastName);
+    List<Author> findByCountry(String country);
 }
