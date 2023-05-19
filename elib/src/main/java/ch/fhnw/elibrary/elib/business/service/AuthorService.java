8b0167814d1e7d77a4a6293c63d64f8ffe2d6762
michimel <126293937+michimel@users.noreply.github.com>
2023-05-19 12:28:58 +0000

    Author controller and service implemented

--- elib/src/main/java/ch/fhnw/elibrary/elib/business/service/AuthorService.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/business/service/AuthorService.java
@@ -1,5 +1,55 @@
 package ch.fhnw.elibrary.elib.business.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import ch.fhnw.elibrary.elib.data.domain.Author;
+import ch.fhnw.elibrary.elib.data.repository.AuthorRepository;
+
+// author @michimel
+
+@Service
 public class AuthorService {
-    
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author createAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author updateAuthor(Long authorID, Author authorDetails) {
+        Author author = getAuthorById(authorID);
+        author.setFirstName(authorDetails.getFirstName());
+        author.setLastName(authorDetails.getLastName());
+        author.setCountry(authorDetails.getCountry());
+        return authorRepository.save(author);
+    }
+
+    public List<Author> getAuthorsByFirstName(String firstName) {
+        return authorRepository.findByFirstName(firstName);
+    }
+
+    public List<Author> getAuthorsByLastName(String lastName) {
+        return authorRepository.findByLastName(lastName);
+    }
+
+    public Author getAuthorByFirstNameAndLastName(String firstName, String lastName) {
+        return authorRepository.findByFirstNameAndLastName(firstName, lastName);
+    }
+
+    public List<Author> getAuthorsByCountry(String country) {
+        return authorRepository.findByCountry(country);
+    }
+
+    private Author getAuthorById(Long authorID) {
+        return authorRepository.findByAuthorID(authorID);
+    }
 }
+
--- elib/src/main/java/ch/fhnw/elibrary/elib/controller/AuthorController.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/controller/AuthorController.java
@@ -1,5 +1,60 @@
 package ch.fhnw.elibrary.elib.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ch.fhnw.elibrary.elib.business.service.AuthorService;
+import ch.fhnw.elibrary.elib.data.domain.Author;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+// author @michimel
+
+@RestController
+@RequestMapping("/api/author")
 public class AuthorController {
-    
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @PostMapping
+    public Author createAuthor(@RequestBody Author author) {
+        return authorService.createAuthor(author);
+    }
+
+    @PutMapping("/{authorID}")
+    public Author updateAuthor(@PathVariable Long authorID, @RequestBody Author author) {
+        return authorService.updateAuthor(authorID, author);
+    }
+
+    @GetMapping("/findByFirstName/{firstName}")
+    public List<Author> getAuthorsByFirstName(@PathVariable String firstName) {
+        return authorService.getAuthorsByFirstName(firstName);
+    }
+
+    @GetMapping("/findByLastName/{lastName}")
+    public List<Author> getAuthorsByLastName(@PathVariable String lastName) {
+        return authorService.getAuthorsByLastName(lastName);
+    }
+
+    @GetMapping("/findByFirstNameAndLastName/{firstName}/{lastName}")
+    public Author getAuthorByFirstNameAndLastName(@PathVariable String firstName, @PathVariable String lastName) {
+        return authorService.getAuthorByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @GetMapping("/findByCountry/{country}")
+    public List<Author> getAuthorsByCountry(@PathVariable String country) {
+        return authorService.getAuthorsByCountry(country);
+    }
 }
