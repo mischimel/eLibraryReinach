13ac7fef8e6a9708f97c1957c0a7e627f5d98793
michimel <126293937+michimel@users.noreply.github.com>
2023-05-19 12:53:05 +0000

    Genre service and controller implemented

--- elib/src/main/java/ch/fhnw/elibrary/elib/business/service/GenreService.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/business/service/GenreService.java
@@ -1,5 +1,41 @@
 package ch.fhnw.elibrary.elib.business.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import ch.fhnw.elibrary.elib.data.domain.Genre;
+import ch.fhnw.elibrary.elib.data.repository.GenreRepository;
+
+// author @michimel
+
+@Service
 public class GenreService {
-    
+
+    private final GenreRepository genreRepository;
+
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    public List<Genre> getAllGenres() {
+        return genreRepository.findAll();
+    }
+
+    public Genre createGenre(Genre genre) {
+        return genreRepository.save(genre);
+    }
+
+    public Genre updateGenre(Long genreId, Genre genreDetails) {
+        Genre genre = getGenreById(genreId);
+        genre.setName(genreDetails.getName());
+        return genreRepository.save(genre);
+    }
+
+    public List<Genre> getGenresByName(String name) {
+        return genreRepository.findByName(name);
+    }
+
+    private Genre getGenreById(Long genreId) {
+        return genreRepository.findByGenreID(genreId);
+    }
 }
+
--- elib/src/main/java/ch/fhnw/elibrary/elib/controller/GenreController.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/controller/GenreController.java
@@ -1,5 +1,46 @@
 package ch.fhnw.elibrary.elib.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ch.fhnw.elibrary.elib.business.service.GenreService;
+import ch.fhnw.elibrary.elib.data.domain.Genre;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+// author @michimel
+
+@RestController
+@RequestMapping("/api/genre")
 public class GenreController {
-    
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<Genre> getAllGenres() {
+        return genreService.getAllGenres();
+    }
+
+    @PostMapping
+    public Genre createGenre(@RequestBody Genre genre) {
+        return genreService.createGenre(genre);
+    }
+
+    @PutMapping("/{genreId}")
+    public Genre updateGenre(@PathVariable Long genreId, @RequestBody Genre genre) {
+        return genreService.updateGenre(genreId, genre);
+    }
+
+    @GetMapping("/findByName/{name}")
+    public List<Genre> getGenresByName(@PathVariable String name) {
+        return genreService.getGenresByName(name);
+    }
 }
+
