470b905b9b3b3b78ee7557423e0e7cc814c96e05
michimel <126293937+michimel@users.noreply.github.com>
2023-05-19 10:14:39 +0000

    UserDetailsServiceImpl adjusted

--- elib/src/main/java/ch/fhnw/elibrary/elib/business/service/UserDetailsServiceImpl.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/business/service/UserDetailsServiceImpl.java
@@ -1,18 +1,16 @@
 package ch.fhnw.elibrary.elib.business.service;
 
-// Class copied from crm webservice project and adapted to elib project
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
 import ch.fhnw.elibrary.elib.data.domain.Member;
 import ch.fhnw.elibrary.elib.data.repository.MemberRepository;
 
-
+   /* Class copied from crm webservice project and adapted to elib project
+    author of adaption @michimel*/
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -22,19 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        
-    // TODO: delete this block when block below works and is uncommented
-    
-       /* UserDetails user = User.builder()
-                            .username(username)
-                            .password("password")
-                            .authorities("READ","ROLE_USER")
-                            .build();   
-        return user;
-        */
-        
-    // TODO: uncomment and check with class Member (crm webservice project it is Agent)
-    
+     
         Member member = memberRepository.findByUsername(username);
         
         if (member == null) {
@@ -47,10 +33,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                                 .authorities("READ","ROLE_" + member.getRole())
                                 .build();
         return user; 
-    
-        
-    }
-
-    
+            
+    }   
     
 }
