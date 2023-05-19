4d693050feae31ad9620e138203c2a5e1fe3bc11
michimel <126293937+michimel@users.noreply.github.com>
2023-05-19 12:56:01 +0000

    Member service and controller implemented

--- elib/src/main/java/ch/fhnw/elibrary/elib/business/service/MemberService.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/business/service/MemberService.java
@@ -1,5 +1,68 @@
 package ch.fhnw.elibrary.elib.business.service;
 
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import ch.fhnw.elibrary.elib.data.domain.Member;
+import ch.fhnw.elibrary.elib.data.repository.MemberRepository;
+
+// author @michimel
+
+@Service
 public class MemberService {
-    
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member createMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public Member updateMember(Long memberId, Member memberDetails) {
+        Member member = getMemberById(memberId);
+        member.setUserName(memberDetails.getUserName());
+        member.setFirstName(memberDetails.getFirstName());
+        member.setLastName(memberDetails.getLastName());
+        member.setEmail(memberDetails.getEmail());
+        member.setPassword(memberDetails.getPassword());
+        member.setRole(memberDetails.getRole());
+        member.setRemember(memberDetails.getRemember());
+        return memberRepository.save(member);
+    }
+
+    public Member getMemberByUserName(String userName) {
+        return memberRepository.findByUserName(userName);
+    }
+
+    public List<Member> getMembersByFirstName(String firstName) {
+        return memberRepository.findByFirstName(firstName);
+    }
+
+    public List<Member> getMembersByLastName(String lastName) {
+        return memberRepository.findByLastName(lastName);
+    }
+
+    public Member getMemberByFirstNameAndLastName(String firstName, String lastName) {
+        return memberRepository.findByFirstNameAndLastName(firstName, lastName);
+    }
+
+    public Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public List<Member> getMembersByRole(String role) {
+        return memberRepository.findByRole(role);
+    }
+
+    private Member getMemberById(Long memberId) {
+        return memberRepository.findByMemberID(memberId);
+    }
 }
+
--- elib/src/main/java/ch/fhnw/elibrary/elib/controller/MemberController.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/controller/MemberController.java
@@ -1,5 +1,71 @@
 package ch.fhnw.elibrary.elib.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ch.fhnw.elibrary.elib.business.service.MemberService;
+import ch.fhnw.elibrary.elib.data.domain.Member;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+// author @michimel
+
+@RestController
+@RequestMapping("/api/member")
 public class MemberController {
-    
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    @PutMapping("/{memberId}")
+    public Member updateMember(@PathVariable Long memberId, @RequestBody Member member) {
+        return memberService.updateMember(memberId, member);
+    }
+
+    @GetMapping("/findByUserName/{userName}")
+    public Member getMemberByUserName(@PathVariable String userName) {
+        return memberService.getMemberByUserName(userName);
+    }
+
+    @GetMapping("/findByFirstName/{firstName}")
+    public List<Member> getMembersByFirstName(@PathVariable String firstName) {
+        return memberService.getMembersByFirstName(firstName);
+    }
+
+    @GetMapping("/findByLastName/{lastName}")
+    public List<Member> getMembersByLastName(@PathVariable String lastName) {
+        return memberService.getMembersByLastName(lastName);
+    }
+
+    @GetMapping("/findByFirstNameAndLastName/{firstName}/{lastName}")
+    public Member getMemberByFirstNameAndLastName(@PathVariable String firstName, @PathVariable String lastName) {
+        return memberService.getMemberByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @GetMapping("/findByEmail/{email}")
+    public Member getMemberByEmail(@PathVariable String email) {
+        return memberService.getMemberByEmail(email);
+    }
+
+    @GetMapping("/findByRole/{role}")
+    public List<Member> getMembersByRole(@PathVariable String role) {
+        return memberService.getMembersByRole(role);
+    }
 }
+
