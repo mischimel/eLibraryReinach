0bbb7458f9b530ba8a35c2c3401afdddb3bf35ed
michimel <126293937+michimel@users.noreply.github.com>
2023-05-21 13:01:10 +0000

    seed database - still some checks to do !

--- elib/src/main/java/ch/fhnw/elibrary/elib/ElibApplication.java
+++ elib/src/main/java/ch/fhnw/elibrary/elib/ElibApplication.java
@@ -1,8 +1,22 @@
 package ch.fhnw.elibrary.elib;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import ch.fhnw.elibrary.elib.data.domain.Book;
+import ch.fhnw.elibrary.elib.data.domain.Genre;
+import ch.fhnw.elibrary.elib.data.domain.Author;
+import ch.fhnw.elibrary.elib.data.domain.Member;
+import ch.fhnw.elibrary.elib.data.domain.Borrowed;
+import ch.fhnw.elibrary.elib.data.repository.AuthorRepository;
+import ch.fhnw.elibrary.elib.data.repository.BookRepository;
+import ch.fhnw.elibrary.elib.data.repository.GenreRepository;
+import ch.fhnw.elibrary.elib.data.repository.MemberRepository;
+import ch.fhnw.elibrary.elib.data.repository.BorrowedRepository;
 
+// author @michimel
 
 @SpringBootApplication
 public class ElibApplication {
@@ -10,5 +24,66 @@ public class ElibApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElibApplication.class, args);
 	}
+
+	@Autowired
+private BookRepository bookRepository;
+
+@Autowired
+private AuthorRepository authorRepository;
+
+@Autowired
+private GenreRepository genreRepository;
+
+@Autowired
+private MemberRepository memberRepository;
+
+@Autowired
+private BorrowedRepository borrowedRepository;
+
+@EventListener(ApplicationReadyEvent.class)
+public void seedDatabase() {
+
+// Pipi Langstrumpf Book, that is borrowed by member(user, not Admin): 
+    // Create Author
+    Author author = new Author();
+    author.setFirstName("Astrid");
+    author.setLastName("Lindgren");
+    author.setCountry("Sweden");
+    authorRepository.save(author);
+
+    // Create Genre
+    Genre genre = new Genre();
+    genre.setName("Children's Fiction");
+    genreRepository.save(genre);
+
+    // Create Book
+    Book book = new Book();
+    book.setIsbn("9781234567890");
+    book.setTitle("Pippi Longstocking");
+    book.setPublishYear(1945);
+    book.setDescription("Astrid Lindgren's classic children's novel");
+    book.setAuthor(author);
+    book.setGenre(genre);
+    bookRepository.save(book);
+
+    // Create Member
+    Member member = new Member();
+    member.setUserName("michele_fille");
+    member.setFirstName("Michele");
+    member.setLastName("Fille");
+    member.setEmail("michele.fille@students.fhnw.ch");
+    member.setPassword("password"); // TODO: check this
+    member.setRole("user");
+   // member.setRemember("yes"); // TODO: check Swagger Schema for this
+    memberRepository.save(member);
+
+    // Create Borrowed
+    Borrowed borrowed = new Borrowed();
+    borrowed.setStatus(true);
+    borrowed.setBook(book);
+    borrowed.setMember(member);
+    borrowedRepository.save(borrowed);
+}
+
 
 }
